f592eb7a91346a94c36ada917fe6410aa5e58e37
Thomas W Nyberg <tomnyberg@gmail.com>
2013-03-06 14:50:42 -0500

    Created: Skeleton file for area computations.

---
+++ src/functional/area.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+"""
+Module for finding area of a polygon.
+"""
+
+def area(vertex_list):
+    """
+    Return area of polygon.
+
+    Parameters
+    ----------
+    vertex_list : list of 1 x 2 np.ndarrays
+
+    Returns
+    -------
+    area : real number
+    """
+    pass
+
+def area_of_triangle(point1, point2, point3):
+    """
+    Return area of triangle.
+
+    Parameters
+    ----------
+    point1 : 1 x 2 np.ndarray
+    point2 : 1 x 2 np.ndarray
+    point3 : 1 x 2 np.ndarray
+
+    Returns
+    -------
+    area : real number
+    """
+    pass
