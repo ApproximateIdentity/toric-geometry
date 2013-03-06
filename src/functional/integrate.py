136ab1b02078d66046a9c43653d27b36deada835
Thomas W Nyberg <tomnyberg@gmail.com>
2013-03-06 11:38:08 -0500

    Create: Made skeleton of integrate file.

---
+++ src/functional/integrate.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+"""
+Module for performing one-dimensional integration of functions over lines in
+the plane and two-dimensional integration of functions over triangles in the
+plane.
+"""
+
+def integrate_over_line(function, point1, point2, weight):
+    """
+    Integrate function on line connecting point1 and point2 in plane and
+    normalize so that the total mass of the line is weight.
+
+    Parameters
+    ----------
+    function : real-valued function whose inputs are 2 x 1 np.ndarrays
+    point1 : 2 x 1 np.ndarray
+    point2 : 2 x 1 np.ndarray
+    weight : postivie real number
+    """
+    pass
+
+def integrate_over_triangle(function, point1, point2, point3):
+    """
+    Integrate function over triangle with vertices given by point1, point2 and
+    point2.
+
+    Parameters
+    ----------
+    function : real-valued function whose inputs are 2 x 1 np.ndarrays
+    point1 : 2 x 1 np.ndarray
+    point2 : 2 x 1 np.ndarray
+    point3 : 2 x 1 np.ndarray
+    """
+    pass
