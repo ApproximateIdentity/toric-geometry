864298beec9a1a9199c88a2787ed2e0e80de0532
Thomas W Nyberg <tomnyberg@gmail.com>
2013-03-06 12:33:47 -0500

    Create: Made skeleton file to compute center of mass

---
+++ src/functional/center_of_mass.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+"""
+Module for finding center of mass of convex polytopes.
+"""
+
+def center_of_mass(vertex_list):
+    """
+    Return center of mass of polygon spanned by vertex list.
+
+    Parameters
+    ----------
+    vertex_list : list of 1 x 2 np.ndarrays
+
+    Returns
+    -------
+    center : 1 x 2 np.ndarray
+    """
+    pass
+
+def center_of_mass_triangle(point1, point2, point3):
+    """
+    Return center of mass of triangle with vertices point1, point2 and point3.
+
+    Parameters
+    ----------
+    point1 : 1 x 2 np.ndarray
+    point2 : 1 x 2 np.ndarray
+    point3 : 1 x 2 np.ndarray
+
+    Returns
+    -------
+    center : 1 x 2 np.ndarray
+    """
+    pass
