953b3440b7403475fe68d3838e8dc460be06305b
Thomas W Nyberg <tomnyberg@gmail.com>
2013-03-06 12:54:40 -0500

    Create: Made skeleton of edge_measure file.

---
+++ src/functional/edge_measure.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+"""
+Module for computing the measures on the edges of a polytope.
+"""
+
+def compute_canonical_measure(triangle_list):
+    """
+    Returns list of canonical measures corresponding to decomposition of a
+    polytope into triangles with base_point the polytope's center of mass.
+
+    Parameters
+    ----------
+    triangle_list : length N list of triples of 1 x 2 np.ndarrays
+
+    Returns
+    -------
+    measures : length N list of real numbers
+    """
+    pass
