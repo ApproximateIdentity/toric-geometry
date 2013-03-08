081fe868295e7f1635ea8de91233b8bd4dc30a99
Thomas W Nyberg <tomnyberg@gmail.com>
2013-03-08 11:46:26 -0500

    Complete: Area works

--- src/functional/area.py
+++ src/functional/area.py
@@ -1,38 +1,43 @@
 import numpy as np
 from numpy import linalg
 
+from src.functional.decompose import decompose_to_triangles
+
 """
 Module for finding area of a polygon.
 """
 
-def area(vertex_list):
+def area(vertex_points):
     """
     Return area of polygon.
 
     Parameters
     ----------
-    vertex_list : N x 2 np.ndarray
+    vertex_points : N x 2 np.ndarray
 
     Returns
     -------
     area : real number
     """
-    area = area_of_triangle(vertex_list)
+    decomposition = decompose_to_triangles(vertex_points)
+    area = 0.0
+    for triangle in decomposition:
+        area += area_of_triangle(triangle)
     return area
 
-def area_of_triangle(vertex_list):
+def area_of_triangle(vertex_points):
     """
     Return area of triangle.
 
     Parameters
     ----------
-    vertex_list : 3 x 2 np.ndarray
+    vertex_points : 3 x 2 np.ndarray
 
     Returns
     -------
     area : real number
     """
     # Use determinant.
-    M = np.vstack((vertex_list.T, np.array([1, 1, 1], dtype='float')))
+    M = np.vstack((vertex_points.T, np.array([1, 1, 1], dtype='float')))
     area = 0.5*linalg.det(M)
     return area
