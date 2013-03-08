024750a68094daf3a29e3d12cf997164cb32b5d3
Thomas W Nyberg <tomnyberg@gmail.com>
2013-03-08 13:04:56 -0500

    Update: Replaced assert_equal with assert_allclose

--- src/functional/tests/test_center_of_mass.py
+++ src/functional/tests/test_center_of_mass.py
@@ -1,6 +1,6 @@
 import unittest
 from numpy import array
-from numpy.testing.utils import assert_equal
+from numpy.testing.utils import assert_allclose
 
 from src.functional.center_of_mass import center_of_mass
 
@@ -18,31 +18,31 @@ class TestDecompose(unittest.TestCase):
         # Make sure center of mass works right for a triangle.
         result = center_of_mass(self.triangle1)
         benchmark = array([1.0/3.0, 1.0/3.0], dtype='float')
-        assert_equal(result, benchmark)
+        assert_allclose(result, benchmark)
 
     def test_com_triangle2(self):
         # Make sure center of mass works right for another triangle.
         result = center_of_mass(self.triangle2)
         benchmark = array([2.0/3.0, 1.0/3.0], dtype='float')
-        assert_equal(result, benchmark)
+        assert_allclose(result, benchmark)
 
     def test_com_square(self):
         # Make sure center of mass works right for a square.
         result = center_of_mass(self.square)
         benchmark = array([1.0/2.0, 1.0/2.0], dtype='float')
-        assert_equal(result, benchmark)
+        assert_allclose(result, benchmark)
 
     def test_com_trapezoid(self):
         # Make sure center of mass works right for a trapezoid.
         result = center_of_mass(self.trapezoid)
         benchmark = array([7.0/9.0, 4.0/9.0], dtype='float')
-        assert_equal(result, benchmark)
+        assert_allclose(result, benchmark)
 
     def test_com_pentagon(self):
         # Make sure center of mass works right for a pentagon.
         result = center_of_mass(self.pentagon)
         benchmark = array([19.0/21.0, 19.0/21.0], dtype='float')
-        assert_equal(result, benchmark)
+        assert_allclose(result, benchmark)
 
 
 if __name__=='__main__':
